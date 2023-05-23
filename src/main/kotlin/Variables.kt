1b936c74c6e94b02c95342c4898c0cc91c2bea54
Влад Никитин <focksikan@gmail.com>
2023-05-23 19:16:48 +0300

    learning Kotlin

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val test1 = "Hello"
+
+    var test2 = "hello"
+    test2 = "Good"
+
+    print("Value of test2 is $test2 ")
+    sum()
+}
+
+fun sum() {
+    val a = 4
+    val b = 543
+    print("Sum is ${a + b}")
+}
