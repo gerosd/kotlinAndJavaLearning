8612ab9770da20756ac438f408d7b3a72ffaf1f0
Влад Никитин <focksikan@gmail.com>
2023-05-31 20:02:09 +0300

    learning Data

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -18,3 +18,13 @@ class Person(private val firstName: String, val surname: String, var age: Int) {
 
     constructor(): this("", "", 0)
 }
+
+class Rectangle(var height: Double, var length: Double) {
+    var perimeter = (height + length) * 2
+
+    var test = 1
+        get() = field + 1 // This
+        set(value) {
+            if (value < 0) println("Negative") else println("Positive")
+        }
+}
